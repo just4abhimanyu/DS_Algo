6feb8d6351d65d7b9d4c79e8f633f44bf91fe626
Abhimanyu Kumar <abhimanyu392kumar@gmail.com>
2025-11-11 23:37:13 +0530

    Unique Number Without Using Collection

--- src/main/java/com/av/ds/arrays/UniqueNumberWithoutUsingCollection.java
+++ src/main/java/com/av/ds/arrays/UniqueNumberWithoutUsingCollection.java
@@ -3,7 +3,10 @@ package com.av.ds.arrays;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+/*
+Find all unique numbers in an array without using any collection framework.
 
+ */
 public class UniqueNumberWithoutUsingCollection {
 
     public static void main(String[] args) {
@@ -16,21 +19,21 @@ public class UniqueNumberWithoutUsingCollection {
     private static int[] findUniqueNumbers(int[] arr) {
 
         int max = Integer.MIN_VALUE;
-        for (int num : arr) {
+        for (int num : arr) { // Find the maximum number in the array
             if (num > max) {
                 max = num;
             }
         }
 
-        int[] count = new int[max + 1];
+        int[] count = new int[max + 1]; // Create a count array to store the frequency of each number
         for (int num : arr) {
-            count[num] = count[num] + 1;
+            count[num] = count[num] + 1; // Increment the count for each number
         }
 
-        List<Integer> uniqueNumbers = new ArrayList<>();
-        for (int j = 0; j < count.length; j++) {
-            if (count[j] == 1) {
-                uniqueNumbers.add(j);
+        List<Integer> uniqueNumbers = new ArrayList<>(); // List to store unique numbers
+        for (int j = 0; j < count.length; j++) { // Find numbers that appear only once
+            if (count[j] == 1) { // If the count is 1, it's a unique number
+                uniqueNumbers.add(j); // Add to the list of unique numbers
             }
         }
         return uniqueNumbers.stream().mapToInt(Integer::intValue).toArray();
